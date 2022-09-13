2c01104564d73dbdb9ce2ed4265b53e0b86193f7
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2022-09-13 16:03:52 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+
+
+
+List<int> ints = Console.ReadLine().Split().Select(int.Parse).ToList();
+string input = Console.ReadLine();
+while (input != "end")
+{
+    switch (input)
+    {
+        case "add":
+            for (int i = 0; i < ints.Count(); i++)
+            {
+                ints[i] += 1;
+            }
+            break;
+        case "multiply":
+            for (int i = 0; i < ints.Count(); i++)
+            {
+                ints[i] *= 2;
+            }
+            break;
+        case "substract":
+            for (int i = 0; i < ints.Count(); i++)
+            {
+                ints[i] -= 1;
+            }
+            break;
+        case "print":
+            Console.WriteLine(String.Join(" ", ints));
+            break;
+        default:
+            break;
+    }
+    input = Console.ReadLine();
+}
